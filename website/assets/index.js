b953fc82a848cb3ab3b2ff1d3f55ec574a65d0c7
adnCode <pedro_adan.deharo31@hotmail.com>
2021-06-03 02:14:12 -0500

    Add node to project

---
+++ website/assets/index.js
@@ -0,0 +1,29 @@
+const video  = document.querySelector('video');
+      const button = document.querySelector('button');
+      button.onclick = () => video.play();
+      function MediaPlayer(config){
+        this.media = config.el;
+      }
+      MediaPlayer.prototype.play = function(){
+        this.media.play();
+      }
+      MediaPlayer.prototype.pause = function(){
+        this.media.pause();
+      }
+
+      
+
+      MediaPlayer.prototype.togglePlay  = function(){
+        if(this.media.paused){
+          this.play();
+        }
+        else{
+          this.pause();
+        }
+      }
+      const player = new MediaPlayer({el: video});
+      
+      button.onclick = () => player.togglePlay();
+      //button.onclick = () => {
+      //  player.media.paused ? player.play() : player.pause();
+      //}
