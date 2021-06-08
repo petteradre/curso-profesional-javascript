fba83ce051ede0d1fba8e97e12ef5c2df7aee2d2
adnCode <pedro_adan.deharo31@hotmail.com>
2021-06-08 18:22:41 -0500

    New excercises

--- website/assets/index.js
+++ website/assets/index.js
@@ -2,9 +2,11 @@ import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './Plugins/AutoPlay.js';
 
       const video = document.querySelector('video');
-      const player = new MediaPlayer({el: video 
-             //plugins:
-             //[new AutoPlay()]
+      const player = new MediaPlayer({el: video, 
+             plugins:
+             [
+                   new AutoPlay()
+             ],
             });
       const button = document.querySelector('button');
       button.onclick = () => player.togglePlay();
